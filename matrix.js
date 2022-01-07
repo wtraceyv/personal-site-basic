9f56a5d53b222f88da37e34123d6ebefa7c8aed9
walter <wtraceyv@gmail.com>
2022-01-06 20:10:11 -0500

    Style and favicons
    - footer copyright message added
    - tweaked sizes of labels and texts
    - added favicons for the most part
    - matrix banner subtext

--- matrix.js
+++ matrix.js
@@ -1,6 +1,7 @@
 // adapted from a small codepen: https://codepen.io/gnsp/pen/vYBQZJm?css-preprocessor=scss
 
 const canvas = document.getElementById('main-matrix');
+const canvasContainer = document.getElementById('matrix-container');
 const ctx = canvas.getContext('2d');
 const w = canvas.width = document.body.offsetWidth;
 const h = canvas.height = document.body.offsetHeight;
@@ -15,7 +16,7 @@ function matrix () {
   ctx.fillRect(0, 0, w, h);
   
   ctx.fillStyle = '#d653ef';
-  ctx.font = '20pt monospace';
+  ctx.font = '22pt monospace';
   
   ypos.forEach((y, ind) => {
     const text = String.fromCharCode(Math.random() * 128);
